a3ef6164696ac90d33af647467536bd47dbe4539
Motoroller89 <41212575+Motoroller89@users.noreply.github.com>
2021-12-05 15:24:55 +0300

    Update FeatureExtracting.py

--- FeatureExtracting.py
+++ FeatureExtracting.py
@@ -1,4 +1,6 @@
-def FeatureExtracting_CN(personData): 
+import math
+
+def FeatureExtracting_CN(personData): #при добавлении условия - бесконечный цикл
     i = 0
     featureData = []
     while i < len(personData['event_type']):
@@ -50,7 +52,7 @@ def FeatureExtracting_MA(PersonData):
     users_temporary = []
     users_sesion =[]
     for i in range(len(PersonData['timestamp'])-1):
-        if int(PersonData['timestamp'][i+1]) - int(PersonData['timestamp'][i])> 3000000:
+        if int(PersonData['timestamp'][i+1]) - int(PersonData['timestamp'][i])> 3000000 and k != 0:
             users_sesion.append(sum(users_temporary)/k)
             users_temporary = []
             k = 0
@@ -77,7 +79,7 @@ def FeatureExtracting_MD(PersonData):
     users_temporary = []
     users_sesion =[]
     for i in range(len(PersonData['timestamp'])-1):
-        if int(PersonData['timestamp'][i+1]) - int(PersonData['timestamp'][i])> 3000000:
+        if int(PersonData['timestamp'][i+1]) - int(PersonData['timestamp'][i])> 3000000 and k != 0:
             users_sesion.append(sum(users_temporary)/k)
             users_temporary = []
             k = 0
@@ -104,7 +106,7 @@ def FeatureExtracting_MS(PersonData):
     users_sesion = []
     users_temporary = []
     for i in range(len(PersonData['timestamp'])-1):
-        if int(PersonData['timestamp'][i+1]) - int(PersonData['timestamp'][i])> 3000000:
+        if int(PersonData['timestamp'][i+1]) - int(PersonData['timestamp'][i])> 3000000 and  len(users_temporary) != 0 :
             users_sesion.append(sum(users_temporary) / len(users_temporary))
             users_temporary = []
         if PersonData['event_type'][i] != '!mousedown' or PersonData['event_type'][i] != '!mouseup':
@@ -128,7 +130,7 @@ def FeatureExtracting_MU(PersonData):
 
 
     for i in range(len(PersonData['timestamp'])-1):
-        if int(PersonData['timestamp'][i+1]) - int(PersonData['timestamp'][i])> 3000000:
+        if int(PersonData['timestamp'][i+1]) - int(PersonData['timestamp'][i])> 3000000 and len(massov_v) != 0:
             average_v = sum(massov_v) / len(massov_v)
             for i in range(len(massov_v)):
                 suma += (massov_v[i] - average_v) ** 2
@@ -166,11 +168,11 @@ def crossing(line1, line2):
     else:
         return False
 
-def FeatureExtracting_ON(PersonData): #проверить правильность работы
+def FeatureExtracting_ON(PersonData):
     i = 0
     k_on = []
     number_intersections = []
-    while i < len(PersonData['event_type']):
+    while i+1 < len(PersonData['event_type']):
         start1 = (float(PersonData['x'][i]),float(PersonData['y'][i]))
         i2 = i
         while PersonData['event_type'][i2] != '!mousedown' and i2+1 < len(PersonData['event_type']):
@@ -193,10 +195,14 @@ def FeatureExtracting_ON(PersonData): #проверить правильност
     return k_on
 
 
-def FeatureExtracting_ME(PersonData): 
+def FeatureExtracting_ME(PersonData):
     i = 0
     driving_efficiency = []
-    while i < len(PersonData['event_type']):
+    date_me = []
+    while i+1 < len(PersonData['event_type']):
+        if int(PersonData['timestamp'][i + 1]) - int(PersonData['timestamp'][i]) > 3000000 and driving_efficiency != []:
+            date_me.append(sum(driving_efficiency) / len(driving_efficiency))
+            driving_efficiency = []
         start1 = (float(PersonData['x'][i]), float(PersonData['y'][i]))
         i2 = i
         k = 0
@@ -207,23 +213,34 @@ def FeatureExtracting_ME(PersonData):
         end1 = (float(PersonData['x'][i2]), float(PersonData['y'][i2]))
         suma = 0
         while i != i2:
-            start2 = ((float(PersonData['x'][i]), float(PersonData['y'][i])))
-            i += 1
-            end2 = (float(PersonData['x'][i]), float(PersonData['y'][i]))
-            suma += ((end2[0]-start2[0])**2 + (end2[1]-start2[1])**2)**0.5
+            if i+1 < len(PersonData['event_type']):
+                if int(PersonData['timestamp'][i + 1]) - int(PersonData['timestamp'][i]) > 3000000 and driving_efficiency != [] :
+                    date_me.append(sum(driving_efficiency) / len(driving_efficiency))
+                    driving_efficiency = []
+                start2 = ((float(PersonData['x'][i]), float(PersonData['y'][i])))
+                i += 1
+                end2 = (float(PersonData['x'][i]), float(PersonData['y'][i]))
+                suma += ((end2[0]-start2[0])**2 + (end2[1]-start2[1])**2)**0.5
+
+            else:
+                i+=1
 
         divider = ((((end1[0]-start1[0])**2) +(end1[1]-start1[1])**2) **0.5)
         if divider != 0:
             driving_efficiency.append(suma / divider)
         i += 1
-    return driving_efficiency
+    return date_me
 
 
 
 def FeatureExtracting_OL(PersonData):
     i = 0
     max_ol = []
-    while i < len(PersonData['event_type']):
+    date_ol = []
+    while i+1 < len(PersonData['event_type']):
+        if int(PersonData['timestamp'][i + 1]) - int(PersonData['timestamp'][i]) > 3000000 and max_ol != []:
+            date_ol.append(sum(max_ol) / len(max_ol))
+            max_ol = []
         i2 = i
         while PersonData['event_type'][i2] != '!mousedown' and i2 + 1 < len(PersonData['event_type']):
             i2 += 1
@@ -232,24 +249,267 @@ def FeatureExtracting_OL(PersonData):
         max_ = (float(PersonData['x'][i2]))
         min_ = max_
         while i != i2:
-            if (float(PersonData['x'][i])) > max_ :
-                if max_ < float(PersonData['x'][i]):
-                    max_ = float(PersonData['x'][i])
-                if min_ > (float(PersonData['x'][i])):
-                    min_ = float(PersonData['x'][i])
-            i += 1
+            if i+1 < len(PersonData['event_type']):
+                if int(PersonData['timestamp'][i + 1]) - int(PersonData['timestamp'][i]) > 3000000 and max_ol != []:
+                    date_ol.append(sum(max_ol) / len(max_ol))
+                    max_ol = []
+                if (float(PersonData['x'][i])) > max_ :
+                    if max_ < float(PersonData['x'][i]):
+                        max_ = float(PersonData['x'][i])
+                    if min_ > (float(PersonData['x'][i])):
+                        min_ = float(PersonData['x'][i])
+                i += 1
+
+            else:
+                i += 1
         if max_ - const > const - min_:
             max_ol.append(max_)
         else:
             max_ol.append(min_)
         i +=1
-    return max_ol
+    return date_ol
+
+
+def FeatureExtracting_MSD_45(PersonData):
+    i = 0
+    users_sesion = []
+    users_temporary = []
+    degres = []
+    while i < len(PersonData['event_type']):
+        myradians = math.atan2(float(PersonData['y'][i]), float(PersonData['x'][i]))
+        mydegrees = math.degrees(myradians)
+        if 0<mydegrees<=45:
+            degres.append(i+1)
+        i +=1
+
+    for j in range(len(degres) - 1):
+        if int(PersonData['timestamp'][j + 1]) - int(PersonData['timestamp'][j]) > 3000000 and len(users_temporary) != 0 :
+            users_sesion.append(sum(users_temporary) / len(users_temporary))
+            users_temporary = []
+        if PersonData['event_type'][j] != '!mousedown' or PersonData['event_type'][j] != '!mouseup':
+            x1, y1 = float(PersonData['x'][j]), float(PersonData['y'][j])
+            time1 = float(PersonData['timestamp'][j])
+            x2, y2 = float(PersonData['x'][j + 1]), float(PersonData['y'][j + 1])
+            time2 = float(PersonData['timestamp'][j + 1])
+            if time1 != time2:
+                ab = (((x2 - x1) ** 2) + ((y2 + y1) ** 2)) ** 0.5
+                v = ab / (time2 - time1)
+                users_temporary.append(v)
+
+
+    return users_sesion
+
+
+def FeatureExtracting_MSD_90(PersonData):
+    i = 0
+    users_sesion = []
+    users_temporary = []
+    degres = []
+    while i < len(PersonData['event_type']):
+        myradians = math.atan2(float(PersonData['y'][i]), float(PersonData['x'][i]))
+        mydegrees = math.degrees(myradians)
+        if 45<mydegrees<=90:
+            degres.append(i+1)
+        i +=1
+
+    for j in range(len(degres) - 1):
+        if int(PersonData['timestamp'][j + 1]) - int(PersonData['timestamp'][j]) > 3000000 and len(users_temporary) != 0 :
+            users_sesion.append(sum(users_temporary) / len(users_temporary))
+            users_temporary = []
+        if PersonData['event_type'][j] != '!mousedown' or PersonData['event_type'][j] != '!mouseup':
+            x1, y1 = float(PersonData['x'][j]), float(PersonData['y'][j])
+            time1 = float(PersonData['timestamp'][j])
+            x2, y2 = float(PersonData['x'][j + 1]), float(PersonData['y'][j + 1])
+            time2 = float(PersonData['timestamp'][j + 1])
+            if time1 != time2:
+                ab = (((x2 - x1) ** 2) + ((y2 + y1) ** 2)) ** 0.5
+                v = ab / (time2 - time1)
+                users_temporary.append(v)
+
+
+    return users_sesion
+
+
+def FeatureExtracting_MSD_135(PersonData):
+    i = 0
+    users_sesion = []
+    users_temporary = []
+    degres = []
+    while i < len(PersonData['event_type']):
+        myradians = math.atan2(float(PersonData['y'][i]), float(PersonData['x'][i]))
+        mydegrees = math.degrees(myradians)
+        if 90<mydegrees<=135:
+            degres.append(i+1)
+        i +=1
+
+    for j in range(len(degres) - 1):
+        if int(PersonData['timestamp'][j + 1]) - int(PersonData['timestamp'][j]) > 3000000 and len(users_temporary) != 0:
+            users_sesion.append(sum(users_temporary) / len(users_temporary))
+            users_temporary = []
+        if PersonData['event_type'][j] != '!mousedown' or PersonData['event_type'][j] != '!mouseup':
+            x1, y1 = float(PersonData['x'][j]), float(PersonData['y'][j])
+            time1 = float(PersonData['timestamp'][j])
+            x2, y2 = float(PersonData['x'][j + 1]), float(PersonData['y'][j + 1])
+            time2 = float(PersonData['timestamp'][j + 1])
+
+            if time1 != time2:
+                ab = (((x2 - x1) ** 2) + ((y2 + y1) ** 2)) ** 0.5
+                v = ab / (time2 - time1)
+                users_temporary.append(v)
+
+
+    return users_sesion
 
 
 
+def FeatureExtracting_MSD_180(PersonData):
+    i = 0
+    users_sesion = []
+    users_temporary = []
+    degres = []
+    while i < len(PersonData['event_type']):
+        myradians = math.atan2(float(PersonData['y'][i]), float(PersonData['x'][i]))
+        mydegrees = math.degrees(myradians)
+        if 135<mydegrees<=180:
+            degres.append(i+1)
+        i +=1
+
+    for j in range(len(degres) - 1):
+        if int(PersonData['timestamp'][j + 1]) - int(PersonData['timestamp'][j]) > 3000000 and len(users_temporary) != 0:
+            users_sesion.append(sum(users_temporary) / len(users_temporary))
+            users_temporary = []
+        if PersonData['event_type'][j] != '!mousedown' or PersonData['event_type'][j] != '!mouseup':
+            x1, y1 = float(PersonData['x'][j]), float(PersonData['y'][j])
+            time1 = float(PersonData['timestamp'][j])
+            x2, y2 = float(PersonData['x'][j + 1]), float(PersonData['y'][j + 1])
+            time2 = float(PersonData['timestamp'][j + 1])
+            if time1 != time2:
+                ab = (((x2 - x1) ** 2) + ((y2 + y1) ** 2)) ** 0.5
+                v = ab / (time2 - time1)
+                users_temporary.append(v)
+
+    return users_sesion
 
 
 
+def FeatureExtracting_MSD_225(PersonData):
+    i = 0
+    users_sesion = []
+    users_temporary = []
+    degres = []
+    while i < len(PersonData['event_type']):
+        myradians = math.atan2(float(PersonData['y'][i]), float(PersonData['x'][i]))
+        mydegrees = math.degrees(myradians)
+        if -135<mydegrees<=-180:
+            degres.append(i+1)
+        i +=1
+
+    for j in range(len(degres) - 1):
+        if int(PersonData['timestamp'][j + 1]) - int(PersonData['timestamp'][j]) > 3000000 and len(users_temporary) != 0:
+            users_sesion.append(sum(users_temporary) / len(users_temporary))
+            users_temporary = []
+        if PersonData['event_type'][j] != '!mousedown' or PersonData['event_type'][j] != '!mouseup':
+            x1, y1 = float(PersonData['x'][j]), float(PersonData['y'][j])
+            time1 = float(PersonData['timestamp'][j])
+            x2, y2 = float(PersonData['x'][j + 1]), float(PersonData['y'][j + 1])
+            time2 = float(PersonData['timestamp'][j + 1])
+            if time1 != time2:
+                ab = (((x2 - x1) ** 2) + ((y2 + y1) ** 2)) ** 0.5
+                v = ab / (time2 - time1)
+                users_temporary.append(v)
+
+
+    return users_sesion
+
+def FeatureExtracting_MSD_270(PersonData):
+    i = 0
+    users_sesion = []
+    users_temporary = []
+    degres = []
+    while i < len(PersonData['event_type']):
+        myradians = math.atan2(float(PersonData['y'][i]), float(PersonData['x'][i]))
+        mydegrees = math.degrees(myradians)
+        if -135<mydegrees<=-90:
+            degres.append(i+1)
+        i +=1
+
+    for j in range(len(degres) - 1):
+         if int(PersonData['timestamp'][j + 1]) - int(PersonData['timestamp'][j]) > 3000000 and len(users_temporary) != 0:
+             users_sesion.append(sum(users_temporary) / len(users_temporary))
+             users_temporary = []
+         if PersonData['event_type'][j] != '!mousedown' or PersonData['event_type'][j] != '!mouseup':
+             x1, y1 = float(PersonData['x'][j]), float(PersonData['y'][j])
+             time1 = float(PersonData['timestamp'][j])
+             x2, y2 = float(PersonData['x'][j + 1]), float(PersonData['y'][j + 1])
+             time2 = float(PersonData['timestamp'][j + 1])
+             if time1 != time2:
+                 ab = (((x2 - x1) ** 2) + ((y2 + y1) ** 2)) ** 0.5
+                 v = ab / (time2 - time1)
+                 users_temporary.append(v)
+
+
+    return users_sesion
+
+
+def FeatureExtracting_MSD_315(PersonData):
+    i = 0
+    users_sesion = []
+    users_temporary = []
+    degres = []
+    while i < len(PersonData['event_type']):
+        myradians = math.atan2(float(PersonData['y'][i]), float(PersonData['x'][i]))
+        mydegrees = math.degrees(myradians)
+        if -90<mydegrees<=-45:
+            degres.append(i+1)
+        i +=1
+
+    for j in range(len(degres)-1):
+
+        if int(PersonData['timestamp'][j + 1]) - int(PersonData['timestamp'][j]) > 3000000 and len(users_temporary) != 0:
+            users_sesion.append(sum(users_temporary) / len(users_temporary))
+            users_temporary = []
+        if PersonData['event_type'][j] != '!mousedown' or PersonData['event_type'][j] != '!mouseup':
+            x1, y1 = float(PersonData['x'][j]), float(PersonData['y'][j])
+            time1 = float(PersonData['timestamp'][j])
+            x2, y2 = float(PersonData['x'][j + 1]), float(PersonData['y'][j + 1])
+            time2 = float(PersonData['timestamp'][j + 1])
+            if time1 != time2:
+                ab = (((x2 - x1) ** 2) + ((y2 + y1) ** 2)) ** 0.5
+                v = ab / (time2 - time1)
+                users_temporary.append(v)
+
+
+    return users_sesion
+
+
+def FeatureExtracting_MSD_360(PersonData):
+    i = 0
+    users_sesion = []
+    users_temporary = []
+    degres = []
+    while i < len(PersonData['event_type']):
+        myradians = math.atan2(float(PersonData['y'][i]), float(PersonData['x'][i]))
+        mydegrees = math.degrees(myradians)
+        if -45<mydegrees<=0:
+            degres.append(i+1)
+        i +=1
+
+    for j in range(len(degres) - 1):
+         if int(PersonData['timestamp'][j + 1]) - int(PersonData['timestamp'][j]) > 3000000 and len(users_temporary) != 0:
+             users_sesion.append(sum(users_temporary) / len(users_temporary))
+             users_temporary = []
+         if PersonData['event_type'][j] != '!mousedown' or PersonData['event_type'][j] != '!mouseup':
+             x1, y1 = float(PersonData['x'][j]), float(PersonData['y'][j])
+             time1 = float(PersonData['timestamp'][j])
+             x2, y2 = float(PersonData['x'][j + 1]), float(PersonData['y'][j + 1])
+             time2 = float(PersonData['timestamp'][j + 1])
+             if time1 != time2:
+                 ab = (((x2 - x1) ** 2) + ((y2 + y1) ** 2)) ** 0.5
+                 v = ab / (time2 - time1)
+                 users_temporary.append(v)
+
+
+    return users_sesion
 
 
 def FeatureExtracting_ALL(data):
@@ -265,7 +525,20 @@ def FeatureExtracting_ALL(data):
         ON = FeatureExtracting_ON(data[i])
         ME = FeatureExtracting_ME(data[i])
         OL = FeatureExtracting_OL(data[i])
-        season.update({'FeatureExtracting_MA': MA, 'FeatureExtracting_MS' : MS,'FeatureExtracting_MU' : MU ,'FeatureExtracting_CD' : CD,'FeatureExtracting_MD' : MD, 'FeatureExtracting_CN' :CN ,
-                       'FeatureExtracting_ON': ON,'FeatureExtracting_ME': ME, 'FeatureExtracting_OL': OL})
+        MSD45 = FeatureExtracting_MSD_45(data[i])
+        MSD90 = FeatureExtracting_MSD_90(data[i])
+        MSD135 = FeatureExtracting_MSD_135(data[i])
+        MSD180 = FeatureExtracting_MSD_180(data[i])
+        MSD225 = FeatureExtracting_MSD_225(data[i])
+        MSD270 = FeatureExtracting_MSD_270(data[i])
+        MSD315 = FeatureExtracting_MSD_315(data[i])
+        MSD360 = FeatureExtracting_MSD_360(data[i])
+        season.update({'Person': [int(i+1)] * max(len(MA),len(MS),len(MU),len(CD),len(MD),len(CN),len(ON),len(ME),len(OL),len(MSD45),len(MSD90),len(MSD135),len(MSD180),
+                                           len(MSD225),len(MSD270),len(MSD315),len(MSD360)),'FeatureExtracting_MA': MA, 'FeatureExtracting_MS' : MS,'FeatureExtracting_MU' : MU ,'FeatureExtracting_CD' : CD,'FeatureExtracting_MD' : MD, 'FeatureExtracting_CN' :CN ,
+                       'FeatureExtracting_ON': ON,'FeatureExtracting_ME': ME, 'FeatureExtracting_OL': OL, 'FeatureExtracting_MSD45': MSD45,
+                       'FeatureExtracting_MSD90': MSD90, 'FeatureExtracting_MSD135': MSD135,'FeatureExtracting_MSD180': MSD180,'FeatureExtracting_MSD225': MSD225,
+                       'FeatureExtracting_MSD270': MSD270,'FeatureExtracting_MSD315': MSD315,'FeatureExtracting_MSD360': MSD360
+                      })
         Feature_users.append(season)
+
     return Feature_users
