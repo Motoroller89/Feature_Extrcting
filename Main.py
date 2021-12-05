66c42e0850f97cf6789b308e6c0d5f4a56ea8a40
Motoroller89 <41212575+Motoroller89@users.noreply.github.com>
2021-12-05 15:25:31 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,15 +2,17 @@ from FileHandler import *
 from FeatureExtracting import *
 
 
+
+
+
+
 def main():
     data = ReadFilesFromDir_CSV('test1')
-
     features = FeatureExtracting_ALL(data)
     WriteFeaturesToFiles_JSON(features)
-
+    #ReadFiles_JSON("features")
     return 0
 
 
 if __name__ == "__main__":
     main()
-
